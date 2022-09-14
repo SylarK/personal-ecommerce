878ed5d2a091d5dfd65f35fbcee8ac472f6e7e3d
Lucas Amado <syst.lucasamado@gmail.com>
2022-09-14 16:19:22 +0100

    Initializing the project

---
+++ backend/src/main/java/pt/amado/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package pt.amado.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
