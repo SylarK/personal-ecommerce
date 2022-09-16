149661a93f257db9211cf3b0affe0ac47f50a1d6
Lucas Amado <syst.lucasamado@gmail.com>
2022-09-16 10:18:09 +0100

    Update ProductRepository.java

--- backend/src/main/java/pt/amado/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/pt/amado/ecommerce/dao/ProductRepository.java
@@ -2,8 +2,10 @@ package pt.amado.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pt.amado.ecommerce.entity.Product;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
