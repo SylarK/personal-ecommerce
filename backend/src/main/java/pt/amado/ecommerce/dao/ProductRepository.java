2bd274c66b7b4d3973136313c2261c87d52b9156
Lucas Amado <syst.lucasamado@gmail.com>
2022-09-26 19:53:54 +0100

    Update ProductRepository.java
    Creating new method (findByCategoryId)

--- backend/src/main/java/pt/amado/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/pt/amado/ecommerce/dao/ProductRepository.java
@@ -1,6 +1,9 @@
 package pt.amado.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import pt.amado.ecommerce.entity.Product;
@@ -8,4 +11,5 @@ import pt.amado.ecommerce.entity.Product;
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
