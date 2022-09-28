7bfab8133b29c0d9d9adb6fb5d55c01fd5ed30af
Lucas Amado <syst.lucasamado@gmail.com>
2022-09-28 18:21:58 +0100

    Update ProductRepository.java
    Creating a new method to get the Products based on the name.

--- backend/src/main/java/pt/amado/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/pt/amado/ecommerce/dao/ProductRepository.java
@@ -11,5 +11,8 @@ import pt.amado.ecommerce.entity.Product;
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
