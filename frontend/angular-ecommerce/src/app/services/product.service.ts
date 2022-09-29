bbc48452708e9cb2cc6dfc8ea052ecb5e0aeb614
Lucas Amado <syst.lucasamado@gmail.com>
2022-09-29 19:17:42 +0100

    Update product.service.ts
    getProduct method that will get a specific product based on theProductId.

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -7,7 +7,7 @@ import { ProductCategory } from '../common/product-category';
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductService {  
   
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category'
@@ -34,9 +34,15 @@ export class ProductService {
   }
 
   private getProducts(searchUrl: string): Observable<Product[]> {
+    
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
+  }
+
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
   }
 }
 
